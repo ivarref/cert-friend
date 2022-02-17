dc1fe872732523db2f3519b6a163d841a409cc02
Ivar Refsdal <ivar.refsdal@nsd.no>
2022-02-17 10:19:14 +0100

    Tools deps project

--- src/com/github/ivarref/cert_friend.clj
+++ src/com/github/ivarref/cert_friend.clj
@@ -1,33 +1,26 @@
-(ns cert-friend.core
+(ns com.github.ivarref.cert-friend
   (:require [clojure.string :as str])
-  (:import (java.security KeyPairGenerator)
-           (java.util.concurrent TimeUnit)
+  (:import (java.util.concurrent TimeUnit)
            (okhttp3.tls HeldCertificate HeldCertificate$Builder)))
-
 
 (defn named-cert [nam cert-str]
   (-> cert-str
       (str/replace "-----BEGIN CERTIFICATE-----" (str "-----BEGIN " nam " CERTIFICATE-----"))
       (str/replace "-----END CERTIFICATE-----" (str "-----END " nam " CERTIFICATE-----"))))
 
-
-(defn root-ca []
-  (let [^KeyPairGenerator kpg (doto (KeyPairGenerator/getInstance "RSA")
-                                (.initialize 2048))
-        ^HeldCertificate rootCertificate (-> (HeldCertificate$Builder.)
+(defn write-certs [{:keys [duration]
+                    :or   {duration 365}}]
+  (let [^HeldCertificate rootCertificate (-> (HeldCertificate$Builder.)
                                              (.certificateAuthority 0)
-                                             (.duration (* 1 365) TimeUnit/DAYS)
-                                             ;(.keyPair (.generateKeyPair kpg))
+                                             (.duration duration TimeUnit/DAYS)
                                              (.build))
         ^HeldCertificate serverCertificate (-> (HeldCertificate$Builder.)
                                                (.signedBy rootCertificate)
-                                               (.duration (* 1 365) TimeUnit/DAYS)
-                                               ;(.keyPair (.generateKeyPair kpg))
+                                               (.duration duration TimeUnit/DAYS)
                                                (.build))
         ^HeldCertificate client (-> (HeldCertificate$Builder.)
                                     (.signedBy rootCertificate)
-                                    (.duration (* 1 365) TimeUnit/DAYS)
-                                    ;(.keyPair (.generateKeyPair kpg))
+                                    (.duration duration TimeUnit/DAYS)
                                     (.build))]
     (spit "server.keys"
           (str (named-cert "ROOT" (.certificatePem rootCertificate))
@@ -37,12 +30,5 @@
     (spit "client.keys"
           (str (named-cert "ROOT" (.certificatePem rootCertificate))
                (named-cert "SELF" (.certificatePem client))
-               (.privateKeyPkcs8Pem client)))
+               (.privateKeyPkcs8Pem client)))))
 
-    (spit "root.crt" (.certificatePem rootCertificate))
-
-    (spit "server.crt" (.certificatePem serverCertificate))
-    (spit "server.pkcs8" (.privateKeyPkcs8Pem serverCertificate))
-
-    (spit "client.crt" (.certificatePem client))
-    (spit "client.pkcs8" (.privateKeyPkcs8Pem client))))
